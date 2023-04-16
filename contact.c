1f223f220ab0a1aae7002f6d51fb9489a6320b45
1SYF666 <99598845+1SYF666@users.noreply.github.com>
2023-04-16 17:11:59 +0800

    Update contact.c

--- contact.c
+++ contact.c
@@ -1,1 +1,58 @@
 
+
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "contact.h"
+
+void InitContact(struct Contact* ps)
+{
+	//将数组中的内容初试化为0，查看CSDN中函数memset使用
+	memset(ps->data, 0, sizeof(ps->data));
+	ps->size = 0;//设置通讯录最初只有0个元素
+}
+
+void AddContact(struct Contact* ps)
+{
+	if (ps->size == MAX)
+	{
+		printf("通讯录已满了,无法增加\n");
+	}
+	else
+	{
+		printf("请输入名字:>");
+		scanf("%s", ps->data[ps->size].name);
+		printf("请输入年龄:>");
+		scanf("%d", &ps->data[ps->size].age);//注意，这里必须取地址，因为age是整型，与前面数组不同
+		printf("请输入性别:>");
+		scanf("%s", ps->data[ps->size].sex);
+		printf("请输入电话:>");
+		scanf("%s", ps->data[ps->size].tele);
+		printf("请输入地址:>");
+		scanf("%s", ps->data[ps->size].addr);
+
+		(ps->size)++;
+		printf("添加成功\n");
+	}
+}
+
+void ShowContact(struct Contact* ps)
+{
+	if (ps->size == 0)
+	{
+		printf("通讯录为空\n");
+	}
+	else
+	{
+		int i = 0;
+		printf("%-20s\t%-4s\t%-5s\t%-12s\t%-20s\n", "名字", "年龄", "性别", "电话", "地址");
+		for (i = 0; i < ps->size; i++)
+		{
+			printf("%-20s\t%-4d\t%-5s\t%-12s\t%-20s\n", 
+				ps->data[i].name,
+				ps->data[i].age,
+				ps->data[i].sex,
+				ps->data[i].tele,
+				ps->data[i].addr);
+		}
+	}
+}

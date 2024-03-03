283a0c402d6498ccfe1314e01ff5aa931c9b1830
1SYF666 <99598845+1SYF666@users.noreply.github.com>
2024-03-03 21:18:41 +0800

    first

---
+++ communicate/QPSK_convcode_sim/compconv.m
@@ -0,0 +1,20 @@
+% Program 3-7
+% compconv.m
+%
+% Function to perform convolution between signal and filter
+%
+% programmed by H.Harada and M.Okita
+%
+
+function [iout, qout] = compconv(idata, qdata, filter)
+
+% **************************************************************** 
+%	idata		: ich data sequcence
+%	qdata		: qch data sequcence
+%   filter      : filter tap coefficience
+% **************************************************************** 
+
+iout = conv(idata,filter);
+qout = conv(qdata,filter);
+ 
+%******************** end of file ***************************

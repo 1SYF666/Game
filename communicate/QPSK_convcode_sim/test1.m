a6b33f754a1a87a51eaf3929e0001bec1fb21904
1SYF666 <99598845+1SYF666@users.noreply.github.com>
2024-03-03 21:18:12 +0800

    second

--- communicate/QPSK_convcode_sim/test1.m
+++ communicate/QPSK_convcode_sim/test1.m
@@ -1,53 +1,119 @@
+
+% Program 3-5
+% qpsk.m
+%
+% Simulation program to realize QPSK transmission system
+%
+% Programmed by H.Harada and T.Yamamura
+%
 clc;
 close all;
 clear;
-%%
+%******************** Preparation part *************************************
 
-% 参数设置
-N = 10000; % 二进制数据点的数量
-Eb_No_dB = 10; % 信噪比（以dB为单位）
+sr=256000.0; % Symbol rate
+ml=2;        % ml:Number of modulation levels (BPSK:ml=1, QPSK:ml=2, 16QAM:ml=4)
+br=sr .* ml; % Bit rate
+nd = 10000;   % Number of symbols that simulates in each loop
+ebn0=-10:8;      % Eb/N0
+IPOINT=8;    % Number of oversamples
 
-% 1. 生成随机二进制数据
-data = randi([0 1], N, 1);
+%************************* Filter initialization ***************************
 
-% 2. QPSK调制
-% 将二进制数据分为两部分，用于QPSK的I和Q两个分支
-dataI = data(1:2:end);
-dataQ = data(2:2:end);
+irfn=21;                  % Number of taps
+alfs=0.5;                 % Rolloff factor
+[xh] = hrollfcoef(irfn,IPOINT,sr,alfs,1);   %Transmitter filter coefficients 
+[xh2] = hrollfcoef(irfn,IPOINT,sr,alfs,0);  %Receiver filter coefficients 
 
-% 映射到QPSK符号
-symbolI = 2*dataI - 1; % 映射到-1和1
-symbolQ = 2*dataQ - 1;
-symbols = symbolI + 1i*symbolQ; % 构造复数QPSK符号
+%******************** START CALCULATION *************************************
 
-% 3. 添加高斯白噪声
-% 计算符号能量
-Es = mean(abs(symbols).^2);
-% 计算比特能量
-Eb = Es/2;
-% 计算噪声方差
-N0 = Eb/(10^(Eb_No_dB/10));
-noiseVariance = N0/2;
-% 生成高斯噪声并加到信号上
-noise = sqrt(noiseVariance)*randn(size(symbols)) + 1i*sqrt(noiseVariance)*randn(size(symbols));
-receivedSymbols = symbols + noise;
+nloop=100;  % Number of simulation loops
 
-% 4. QPSK解调
-receivedI = real(receivedSymbols) > 0;
-receivedQ = imag(receivedSymbols) > 0;
+noe = 0;    % Number of error data
+nod = 0;    % Number of transmitted data
 
-% 将I和Q部分的解调数据重新组合成二进制序列
-receivedData = zeros(2*numel(receivedI), 1);
-receivedData(1:2:end) = receivedI;
-receivedData(2:2:end) = receivedQ;
+for kkk=1:length(ebn0)
+    ber222 = zeros(1,nloop);
+    for iii=1:nloop
 
-% 5. 计算误码率（BER）
-errors = sum(data ~= receivedData);
-BER = errors/N;
+    %*************************** Data generation ********************************  
 
-% 显示结果
-fprintf('BER = %f\n', BER);
+        data1=rand(1,nd*ml)>0.5;  % rand: built in function
 
-% 画图
-figure;plot(real(symbols));title('原信号实部');
-figure;plot(real(receivedSymbols));title('加噪后信号实部');
+    %*************************** QPSK Modulation ********************************  
+
+        [ich,qch]=qpskmod(data1,1,nd,ml);
+        [ich1,qch1]= compoversamp(ich,qch,length(ich),IPOINT); 
+        [ich2,qch2]= compconv(ich1,qch1,xh); 
+
+    %**************************** Attenuation Calculation ***********************
+        spow=sum(ich2.*ich2+qch2.*qch2)/nd;  % sum: built in function
+        attn=0.5*spow*sr/br*10.^(-ebn0(kkk)/10);
+        attn=sqrt(attn);  % sqrt: built in function
+
+    %********************** Fading channel **********************
+
+      % Generated data are fed into a fading simulator
+      % [ifade,qfade]=sefade(ich2,qch2,itau,dlvl,th1,n0,itnd1,now1,length(ich2),tstp,fd,flat);
+
+      % Updata fading counter
+      %itnd1 = itnd1+ itnd0;
+
+    %********************* Add White Gaussian Noise (AWGN) **********************
+
+        [ich3,qch3]= comb(ich2,qch2,attn);% add white gaussian noise
+        
+%     %******************** awgn函数加噪 ******************%   
+%         snr(kkk) =ebn0(kkk)+10*log10(br/sr)-10 * log10(IPOINT) ; 
+%         ch3 = awgn(ich2 + 1i*qch2,snr(kkk),'measured');
+%         ich3 = real(ch3);
+%         qch3 = imag(ch3);
+        
+        [ich4,qch4]= compconv(ich3,qch3,xh2);
+
+        syncpoint=irfn*IPOINT+1;
+        ich5=ich4(syncpoint:IPOINT:length(ich4));
+        qch5=qch4(syncpoint:IPOINT:length(qch4));
+
+    %**************************** QPSK Demodulation *****************************
+
+        [demodata]=qpskdemod(ich5,qch5,1,nd,ml);
+
+    %************************** Bit Error Rate (BER) ****************************
+
+        noe2=sum(abs(data1-demodata));  % sum: built in function
+        nod2=length(data1);  % length: built in function
+        noe=noe+noe2;
+        nod=nod+nod2;
+
+%         fprintf('%d\t%e\n',iii,noe2/nod2);  % fprintf: built in function
+        ber222(iii) = noe2/nod2;
+    end % for iii=1:nloop   
+    
+%     ber(kkk) = noe/nod;
+    ber(kkk)= mean(ber222);
+    fprintf('%d\t%e\n',kkk,ber(kkk));
+end
+%********************** Output result ***************************
+
+% ber = noe/nod;
+% fprintf('%d\t%d\t%d\t%e\n',ebn0,noe,nod,noe/nod);  % fprintf: built in function
+% fid = fopen('BERqpsk.dat','a');
+% fprintf(fid,'%d\t%e\t%f\t%f\t\n',ebn0,noe/nod,noe,nod);  % fprintf: built in function
+% fclose(fid);
+
+%******************** end of file ***************************
+ber_theory = 0.5*erfc(sqrt(10.^(ebn0/10)));
+semilogy(ebn0,ber,'-*',ebn0,ber_theory,'-+');
+xlabel('比特信噪比');
+ylabel('误码率');
+title('不同信噪比下误码率仿真曲线');
+legend('实验曲线','理论曲线');
+grid on;
+
+
+
+
+
+
+

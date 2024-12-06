d28398fb9235260521d43032ca5a7684c5cd483b
Shen Yifu <99598845+1SYF666@users.noreply.github.com>
2024-12-06 20:36:21 +0800

    Delete communicate directory

--- communicate/GMSK_sim/function/Gmsk_main.m
+++
@@ -1,8 +0,0 @@
-clc;clear;
-close all;
-%%
-LOOPS = 1;
-Lh = 4;
-GSMsim_demo(LOOPS,Lh);
-
-%%
--- communicate/convolutionalcode_sim/metric_hard.m
+++
@@ -1,9 +0,0 @@
-
-function [ distance ] = metric_hard( x,y )
-% 硬判决与汉明距测量
-if x == y
-    distance = 0;
-else
-    distance = 1;
-end
-end

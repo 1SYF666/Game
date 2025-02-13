e3fd6920b3b8f9e85f5ba593488fc438cbb1645e
1SYF666 <99598845+1SYF666@users.noreply.github.com>
2025-02-13 21:29:38 +0800

    add new some code

--- socket/main.cpp
+++ socket/main.cpp
@@ -11,11 +11,34 @@ int main()
 	// 创建套接字
 	SOCKET servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	// 绑定套戒指
+	// 绑定套接字
+	struct sockaddr_in sockAddr;
+	memset(&sockAddr, 0, sizeof(sockAddr));
+	sockAddr.sin_family = AF_INET;
+	sockAddr.sin_addr.s_addr = inet_addr("192.168.1.102");
+	sockAddr.sin_port = htons(1234);
+	bind(servSock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
+
+	// 进入监听状态
+	listen(servSock, 20);
+
+	// 接受客户端请求
+	SOCKADDR clntAddr;
+	int nSize = sizeof(SOCKADDR);
+	SOCKET clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
+
+	// 向客户端请求：
+	const char* str = "hello world!";
+	send(clntSock, str, strlen(str) + sizeof(char), NULL);
+
+	// 关闭套接字
+	closesocket(clntSock);
+	closesocket(servSock);
 
 
+	// 终止DLL的使用
 
-
+	WSACleanup();
 
 	return 0;
 }

e10606f382bd7db0cb51bf13e874fa21635b0d79
Shen Yifu <99598845+1SYF666@users.noreply.github.com>
2025-02-17 22:45:09 +0800

    add some new code

--- socket/client/mian.cpp
+++ socket/client/mian.cpp
@@ -1,28 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<WinSock2.h>
-
+#include"util.h"
 #pragma comment(lib,"ws2_32.lib")
 
 int main()
 {
 	//初始化DLL
 	WSADATA wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	//WSAStartup(MAKEWORD(2, 2), &wsaData);
+	errif(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0, "Winsock 初始化失败!");
 
 	// 创建套接字
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
+	errif(sock == INVALID_SOCKET, "套接字创建失败!");
+	
 	//向服务器发起请求
 	struct sockaddr_in sockAddr;
 	memset(&sockAddr, 0, sizeof(sockAddr));
 	
 	sockAddr.sin_family = AF_INET;
-	sockAddr.sin_addr.s_addr = inet_addr("192.168.1.102");
+	sockAddr.sin_addr.s_addr = inet_addr("192.168.1.101");
 	sockAddr.sin_port = htons(1234);
 
-	connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
-
+	errif(connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR))== SOCKET_ERROR,"连接客户端失败！");
+	
+	printf("客户端连接服务器成功\n");
 	// 接收服务器传回的数据
 	char szBuffer[MAXBYTE] = { 0 };
 	recv(sock, szBuffer, MAXBYTE, NULL);
@@ -40,3 +43,4 @@ int main()
 
 	return 0;
 }
+

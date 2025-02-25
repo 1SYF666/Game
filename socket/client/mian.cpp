861a418e433ec5f6577705d4c59d4f359a6ebd72
1SYF666 <99598845+1SYF666@users.noreply.github.com>
2025-02-25 21:00:35 +0800

    modify some notation

--- socket/client/mian.cpp
+++ socket/client/mian.cpp
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<WinSock2.h>
 #include"util.h"
+#include<iostream>
 #pragma comment(lib,"ws2_32.lib")
 
 int main()
@@ -9,30 +10,51 @@ int main()
 	//初始化DLL
 	WSADATA wsaData;
 	//WSAStartup(MAKEWORD(2, 2), &wsaData);
-	errif(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0, "Winsock 初始化失败!");
+	errif(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0, "Winsock initialization failed!");
 
 	// 创建套接字
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	errif(sock == INVALID_SOCKET, "套接字创建失败!");
+	errif(sock == INVALID_SOCKET, "Socket creation failed!");
 	
 	//向服务器发起请求
 	struct sockaddr_in sockAddr;
 	memset(&sockAddr, 0, sizeof(sockAddr));
 	
 	sockAddr.sin_family = AF_INET;
-	sockAddr.sin_addr.s_addr = inet_addr("192.168.1.101");
+	sockAddr.sin_addr.s_addr = inet_addr("192.168.0.148");
 	sockAddr.sin_port = htons(1234);
 
 	errif(connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR))== SOCKET_ERROR,"连接客户端失败！");
 	
-	printf("客户端连接服务器成功\n");
-	// 接收服务器传回的数据
-	char szBuffer[MAXBYTE] = { 0 };
-	recv(sock, szBuffer, MAXBYTE, NULL);
+	printf("Server socket bind successful\n");
+	// 接收服务器传回的数据	
+	// char szBuffer[MAXBYTE] = { 0 };
+	// recv(sock, szBuffer, MAXBYTE, NULL);
 
-	// 输出接收到的数据
-	printf("Message from server:%s\n", szBuffer);
+	// // 输出接收到的数据
+	// printf("Message from server:%s\n", szBuffer);
 
+	while(true){
+        char buf[1024];
+        memset(buf, 0x00,sizeof(buf));
+		std::cin>>buf;
+        ssize_t write_bytes = send(sock, buf, sizeof(buf),NULL);
+        if(write_bytes == -1){
+            printf("socket already disconnected, can't write any more!\n");
+            break;
+        }
+        memset(buf, 0x00,sizeof(buf));
+        ssize_t read_bytes = recv(sock, buf, sizeof(buf),NULL);
+        if(read_bytes > 0){
+            printf("message from server: %s\n", buf);
+        }else if(read_bytes == 0){
+            printf("server socket disconnected!\n");
+            break;
+        }else if(read_bytes == -1){
+            closesocket(sock);
+            errif(true, "socket read error");
+        }
+    }
 	// 关闭套接字
 	closesocket(sock);
 
